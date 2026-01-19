0b7e64c0d1c1bc1ca6fa2d94869612cc74e390a8
adarshsvg <adarshkle@gmail.com>
2026-01-19 11:08:08 +0530

    Implement division with zero check in math4.pyhlo
    Added division operation with zero check to math4.py

--- math4.py
+++ math4.py
@@ -3,5 +3,10 @@ try:
     b = int(input("Enter the second number: "))
     print(f"Addition = {a + b}")
     print(f"Subtraction = {a - b}")
+    
+    if b != 0:
+        print(f"Division = {a / b}")
+    else:
+        print("Division by zero is not allowed")
 except ValueError:
     print("Enter a valid number!")
